ca928d8f79b8f725a0b3face7b29bbfd60214134
ShahnaBasheer <shahana.basheer.in@gmail.com>
2024-01-08 20:15:07 +0530

    razorpay module issue has been sorted

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const dbConnect = () => {
     try{
-        const conn = mongoose.connect(process.env.MONGODB_URL);//dbname
+        const conn = mongoose.connect(process.env.MONGODB_URL,{ useNewUrlParser: true, useUnifiedTopology: true });//dbname
         console.log("Database connected successfull!");
     } catch(error){
         console.log("Databse Error");
